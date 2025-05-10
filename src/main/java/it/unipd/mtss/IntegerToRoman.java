59d5ce324f16ac983db133741764a3ec44165da7
zakka00 <zaka.laoud.2004@gmail.com>
2025-05-10 16:47:46 +0200

    Conversione primi 100 numeri interi arabi in romani Closes #7

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,13 +6,13 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
     public static String convert(int number) {
-        if (number < 1 || number > 50) {
-            throw new IllegalArgumentException("Number must be between 1 and 50");
+        if (number < 1 || number > 100) {
+            throw new IllegalArgumentException("Number must be between 1 and 100");
         }
 
-        String[] decine = {"", "X", "XX", "XXX", "XL", "L"};
-        String[] unita = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-        
+        String[] decine = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"};
+        String[] unita =  {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
         return decine[number / 10] + unita[number % 10];
     }
 }
--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class IntegerToRomanTest {
-    
     @Test
     public void testPrimiTreNumeri() {
         IntegerToRoman i = new IntegerToRoman();
@@ -12,7 +11,6 @@ public class IntegerToRomanTest {
         assertEquals("II", i.convert(2));
         assertEquals("III", i.convert(3));
     }
-    
     @Test
     public void testPrimiSeiNumeri() {
         IntegerToRoman i = new IntegerToRoman();
@@ -20,7 +18,6 @@ public class IntegerToRomanTest {
         assertEquals("V", i.convert(5));
         assertEquals("VI", i.convert(6));
     }
-
     @Test
     public void testPrimiDieciNumeri() {
         IntegerToRoman i = new IntegerToRoman();
@@ -29,7 +26,6 @@ public class IntegerToRomanTest {
         assertEquals("IX", i.convert(9));
         assertEquals("X", i.convert(10));
     }
-
     @Test
     public void testPrimiVentiNumeri() {
         IntegerToRoman i = new IntegerToRoman();
@@ -38,7 +34,6 @@ public class IntegerToRomanTest {
         assertEquals("XIX", i.convert(19));
         assertEquals("XVII", i.convert(17));
     }
-
     @Test
     public void testPrimiCinquantaNumeri() {
         IntegerToRoman i = new IntegerToRoman();
@@ -49,10 +44,24 @@ public class IntegerToRomanTest {
         assertEquals("XXXIX", i.convert(39));
         assertEquals("XL", i.convert(40));
     }
-
+    @Test
+    public void testNumeriDaCinquantaACento() {
+        IntegerToRoman i = new IntegerToRoman();
+        assertEquals("L", i.convert(50));
+        assertEquals("LI", i.convert(51));
+        assertEquals("LIX", i.convert(59));
+        assertEquals("LX", i.convert(60));
+        assertEquals("LXX", i.convert(70));
+        assertEquals("LXXX", i.convert(80));
+        assertEquals("XC", i.convert(90));
+        assertEquals("XCIX", i.convert(99));
+        assertEquals("C", i.convert(100));
+    }
     @Test(expected = IllegalArgumentException.class)
     public void testOutOfBounds() {
-        IntegerToRoman.convert(-10);
-        IntegerToRoman.convert(55);
+        IntegerToRoman.convert(-1);
+        IntegerToRoman.convert(0);
+        IntegerToRoman.convert(101);
+        IntegerToRoman.convert(103);
     }
 }
