bc0c8ab2da8b4ce9fe0568b21a15d6ad25cf21e7
Edis <edis.hodja@studenti.unipd.it>
2025-05-10 15:48:31 +0200

    Conversione primi 3 numeri interi arabi in romani Closes #2

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,7 +6,14 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
     public static String convert(int number){
-        // TODO
-        return null;
+        String result = "";
+        if (number < 1 || number > 3) {
+            throw new IllegalArgumentException("Number out of bounds");
+        }
+        while (number > 0) {
+            result += "I";
+            number--;
+        }
+        return result;
     }
 }
--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -1,11 +1,21 @@
 package it.unipd.mtss;
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class IntegerToRomanTest {
+    
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testPrimiTreNumeri() {
+        IntegerToRoman i = new IntegerToRoman();
+        assertEquals("I", i.convert(1));
+        assertEquals("II", i.convert(2));
+        assertEquals("III", i.convert(3));
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testOutOfBounds() {
+        IntegerToRoman.convert(-10);
+        IntegerToRoman.convert(5);
     }
 }
